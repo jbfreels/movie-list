34b716836c30ebaba4137d48465c536be6f550a2
JB Freels <jbfreels@me.com>
2019-12-07 12:52:11 -0500

    Added core/Config class
    - config.yml added so no more hard coded paths in main

---
+++ core/Config.py
@@ -0,0 +1,17 @@
+import yaml
+import json
+
+class Config:
+  movie_path = ""
+  movie_exts = ""
+  total = False
+
+  def __init__ (self, path="config.yml"):
+    with open ("config.yml", 'r') as yml:
+      self.yaml = yaml.load (yml, Loader=yaml.Loader)
+      self.__dict__.update (self.yaml)
+
+  def __str__ (self):
+    return json.dumps(self.yaml)
+
+config = Config ()
--- movie-list.py
+++ movie-list.py
@@ -1,17 +1,16 @@
 from glob import glob
 from os import path
+import json
 
 from core.Movie import Movie
-
-MOVIE_PATH = "/mnt/lilnas/movies"
-MOVIE_EXTS = ("*.avi", "*.mp4", "*.m4v", "*.mkv")
+from core.Config import config
 
 def get_movies ():
   files = []
   movies = []
   
-  for ext in MOVIE_EXTS:
-    files.extend (glob (path.join (MOVIE_PATH, ext)))
+  for ext in config.movie_exts:
+    files.extend (glob (path.join (config.movie_path, ext)))
 
   for file in files:
     movies.append (Movie (file))
@@ -24,4 +23,5 @@ if __name__ == '__main__':
   for movie in movies:
     print ("{}: {}".format (movie.displayname, movie.year))
 
-  print ("{}\nmovies found: {}".format ('-' * 55, len(movies)))
+  if config.total:
+    print ("{}\nmovies found: {}".format ('-' * 55, len(movies)))
